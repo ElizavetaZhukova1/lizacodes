666a4301e18f99e7e4b6d669b361e7f3eb1006b8
ElizavetaZhukova1 <elizavetazhukova57@gmail.com>
2024-02-05 16:54:54 -0800

    hat  bring iilite desktop no animations

---
+++ src/components/HiiliteBring.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import codeBackground from '../assets/Code_bg_brighter.png'; // Make sure this path is correct
+
+const HiiliteBring = () => {
+  return (
+    <div className="bg-white_bg font-body_text text-black_bg py-6">
+     <div className="relative font-head_text text-6xl text-center mb-4">
+      <span className="text-blue_light relative text-8xl" style={{ left: '0.37em', zIndex: 10 }}>W</span>
+      <span className="text-black_bg relative" style={{ zIndex: 20 }}>What will I bring to Hiilite?</span>
+    </div>
+    
+    <div className="flex justify-around items-start space-x-8 mx-10 mt-10 mb-4 text-black_bg">
+        <div className="text-center space-y-4 mx-6">
+            <h3 className="font-head_text text-3xl mb-2">Clean code</h3>
+            <div className="bg-auto h-1.5 w-[60%] mx-auto" style={{ backgroundImage: `url(${codeBackground})` }} />
+            <p className="font-body_text text-xl text-left text-wrap mt-2">In my previous company, my quick issue resolution led to a 5% increase in team productivity, my peer reviews reduced the amount of mistakes by 10% and my strive to clean up the code base decreased code repetition by 8%.</p>
+        </div>
+        <div className="text-center space-y-4 mx-6">
+            <h3 className="font-head_text text-3xl mb-2">In-depth analysis</h3>
+            <div className="bg-auto h-1.5 w-[60%] mx-auto" style={{ backgroundImage: `url(${codeBackground})` }} />
+            <p className="font-body_text text-xl text-left text-wrap mt-2">In my Capstone Project I Managed Quality Assurance for both backend and frontend, reducing bugs by 50% and boosting system performance by 30%.I am also well-versed in a multitude of data science tools such as Python, R, Tableau, Excel.</p>
+        </div>
+        <div className="text-center space-y-4 mx-6">
+            <h3 className="font-head_text text-3xl mb-2">Inspiring presentations</h3>
+            <div className="bg-auto h-1.5 w-[60%] mx-auto" style={{ backgroundImage: `url(${codeBackground})` }} />
+            <p className="font-body_text text-xl text-left text-wrap mt-2">In my previous company I created a data preprocessing system to analyze team performance, and spearheaded the presentations for the clients, reducing project delays by 25% and improving client updates.</p>
+        </div>
+    </div>
+    
+     
+    </div>
+  );
+};
+
+export default HiiliteBring;
--- src/pages/ForHiilite.js
+++ src/pages/ForHiilite.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import TopContact from '../components/TopContact';
 import HiiliteIntro from '../components/HiiliteIntro';
+import HiiliteBring from "../components/HiiliteBring"
 
 const ForHiilite = () => (
   <div>
      <TopContact />
     <HiiliteIntro />
+    <HiiliteBring />
   </div>
 );
 
